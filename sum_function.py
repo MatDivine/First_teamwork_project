0e29d7ac9f8bb680e95953cca761c2537d3f3c63
haniyeh <haniyeh.gbs@outlook.com>
2024-11-08 15:35:39 +0330

    i created sum function

--- sum_function.py
+++ sum_function.py
@@ -1,1 +1,11 @@
 # You need to create a sum function that takes two numbers as input to the function and add the first number to the second number.
+def sum_function(num1, num2):
+    return num1 + num2
+
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+
+result = sum_function(first_number, second_number)
+print(result)
