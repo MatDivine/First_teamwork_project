5b2e04e5ca1e962400aab6623abcf03279e1a64e
Mohammadreza Khodaei <khodaeimhmdreza@gmail.com>
2024-11-08 15:15:16 +0330

    i created multiplication function

--- multiplication_function.py
+++ multiplication_function.py
@@ -1,1 +1,3 @@
 # You need to create a multipliction function that takes two numbers as input to the function and multiply the first number by the second number.
+def multipliction(x, y):
+    return x * y
