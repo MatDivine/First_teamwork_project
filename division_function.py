98b10c7b81258ff97ee08ec4bff1c8ce852328ed
Pooya Danandeh <pooya.dan76@gmail.com>
2024-11-08 15:00:55 +0330

    division function completed

--- division_function.py
+++ division_function.py
@@ -1,6 +1,5 @@
 # You need to create a divide function that takes two numbers as input to the function and divides the first number by the second.
 
-
-
-
-
+def divide(*, a:float, b:float) -> float:
+    '''divide `a` by `b` -> `a/b`'''
+    return a / b
